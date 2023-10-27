4e25b725800fef9dce6020e5e9c1c5c808000407
AgusIglesias72 <enpalabrass@gmail.com>
2023-10-27 04:25:11 -0300

    add

---
+++ src/app/api/landing/route.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { appendData } from '@/connections/Google'
+
+const corsHeaders = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+}
+
+export async function OPTIONS(req: NextRequest, res: NextResponse) {
+  return NextResponse.json({}, { headers: corsHeaders })
+}
+
+export async function POST(req: NextRequest, res: NextResponse) {
+  req.headers.set('Access-Control-Allow-Origin', '*')
+
+  try {
+    const data = await req.json()
+
+    const response = await appendData(data)
+
+    return new NextResponse(JSON.stringify({ message: 'Ok' }), {
+      status: 200,
+      headers: corsHeaders,
+    })
+  } catch (error) {
+    console.log(error)
+    return new NextResponse(JSON.stringify({ message: 'Error' }), {
+      status: 500,
+    })
+  }
+}
