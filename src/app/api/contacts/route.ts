e75bcdf62dbb6204b9679ea5f88a660049e2b279
AgusIglesias72 <enpalabrass@gmail.com>
2023-11-07 12:58:51 -0300

    add all

--- src/app/api/contacts/route.ts
+++ src/app/api/contacts/route.ts
@@ -10,9 +10,21 @@ type ListType = {
   name: string
 }
 
-const PERFIT_LIST: number = 2
+const PERFIT_LIST: number = 67
+
+const corsHeaders = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+}
+
+export async function OPTIONS(req: NextRequest, res: NextResponse) {
+  return NextResponse.json({}, { headers: corsHeaders })
+}
 
 export async function POST(req: NextRequest, res: NextResponse) {
+  req.headers.set('Access-Control-Allow-Origin', '*')
+
   try {
     const email: Payload = await req.json()
 
